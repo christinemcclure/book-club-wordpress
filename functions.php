68d29794ae48a492d769104232b049c6ea7cdf6d
Christine McClure <camcclure@gmail.com>
2018-02-05 18:39:59 -0600

    enqueue parent style

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
-// load css into the website's front-end
-function kotha_enqueue_style() {
-    wp_enqueue_style( 'kotha-style', get_stylesheet_uri() ); 
+<?php
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'kotha', get_template_directory_uri() . '/style.css' );
 }
-add_action( 'wp_enqueue_scripts', 'kotha_enqueue_style' );
